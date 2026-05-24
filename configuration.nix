6b2707802309b5a4ac127635fdc022bc49315fb2
tokime <artem@tokime.ru>
2026-05-25 01:15:12 +0300

    .

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.devices = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelParams = [ "nvidia-drm.modeset=1" "nvidia-drm.fbdev=1" ];
   boot.initrd.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm"];
